886f2a65214532255896a78257b0830ca9a04256
Joao <cardoso@elipse.com.br>
2017-10-31 17:28:16 -0200

    third

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log('Got request for ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('<h1>Hello Code and Azure Web Apps!</h1>');
-// }).listen(process.env.PORT);
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+}).listen(process.env.PORT);
 
-var express = require('express')
-  , app = express()
-  , server = require('http').Server(app)
+// var express = require('express')
+//   , app = express()
+//   , server = require('http').Server(app)
 
 
 
@@ -16,11 +16,11 @@ var express = require('express')
 /////////////////////////////////////////
 // Http Server
 /////////////////////////////////////////
-server.listen(80, function (err) {
-  if (err) console.log(err);
-  else console.log("Express server is listen to port 80!");
-});
-app.use(express.static('www'));
+// server.listen(80, function (err) {
+//   if (err) console.log(err);
+//   else console.log("Express server is listen to port 80!");
+// });
+// app.use(express.static('www'));
 
 //daozinho danado demais
 
