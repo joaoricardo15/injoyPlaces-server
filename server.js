370ff2e5b95a18613275218145e4fb8e14cbbc7e
joaoricardo15 <joaoricardo15@hotmail.com>
2017-10-31 20:02:37 -0200

    daozera

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
-}).listen(process.env.PORT);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     console.log('Got request for ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+// }).listen(process.env.PORT);
 
-// var express = require('express')
-//   , app = express()
-//   , server = require('http').Server(app)
+var express = require('express')
+  , app = express()
+  , server = require('http').Server(app);
 
 
 
-//
 
-/////////////////////////////////////////
-// Http Server
-/////////////////////////////////////////
-// server.listen(80, function (err) {
-//   if (err) console.log(err);
-//   else console.log("Express server is listen to port 80!");
-// });
-// app.use(express.static('www'));
+
+///////////////////////////////////////
+//Http Server
+///////////////////////////////////////
+server.listen(80, function (err) {
+  if (err) console.log(err);
+  else console.log("Express server is listen to port 80!");
+});
+app.use(express.static('www'));
 
 //daozinho danado demais
 
