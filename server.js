628f32b13d3f7e99d0a45a519afc50882242359f
joaoricardo15 <joaoricardo15@hotmail.com>
2018-04-30 23:48:08 -0300

    forth

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var express = require('express')
 
 app.use(express.static('www'));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});    
 
 var positions = [];
 
