8d42173f59c07f3642e025300d664df8c2efdcc0
Cardoso <cardoso@elipse.com.br>
2019-04-09 10:07:01 -0300

    bugs resolved for first azure setup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
   , bodyParser = require('body-parser')
   , app = express()
-  , port = 80
+  , env = { Local: 0, Azure: 1 }
+  , envMode = env.Azure
+  , port
   , geoLocations = [];
 
 app.use(express.static('www'));
