5c15fc662a12d668b38976b601a3479bce6bb22f
Cardoso <cardoso@elipse.com.br>
2019-05-28 17:42:32 -0300

    beckend first try

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+  , file = require('fs')
   , bodyParser = require('body-parser')
   , app = express()
   , env = { Local: 0, Azure: 1 }
@@ -6,12 +7,12 @@ var express = require('express')
   , port
 
 app.use(express.static('www'))
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '5mb'})) //app.use(bodyParser.urlencoded({ extended: false }))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
-});    
+});
 
 /*/////////////////// Conversão de precisão em graus para distancia /////////////////////
 
@@ -37,12 +38,43 @@ var longitudeThreshold = 0.00042
 var localMinimunInterval = 600
 
 app.post('/positions', function(req, res) {
-	
-	for (let i = 0; i < req.body.length; i++) {
-		addLocation(req.body[i])
-	}
-		
+	for (let i = 0; i < req.body.length; i++) { addLocation(req.body[i]) }
     res.send({ message: 'É uz Guri' })
+})
+
+//var multer = require('multer');
+//var upload = multer({ dest: '/tmp' })
+
+var role = { name: null, pic: null }
+
+app.post('/roles', (req, res) => { 
+	role.name = req.body.name
+	role.pic = req.body.pic
+	console.log('name: ' + role.name)
+
+	let newRole = new RoleModel({name: req.body.name, img: { data: req.body.pic, contentType: 'image/png' } });
+
+	newRole.save(function(err) { if (err) throw err })
+
+	res.send({ message: "Éh uz Guri"})
+})
+
+app.get('/roles', function(request, response) {
+	RoleModel.find({ name: 'Ap11' }, function(err, res) {
+		if (res) {
+			let role = res[0]
+			console.log('role name: ' + role.name)
+			//response.contentType(role.img.contentType)
+			//response.send(role.img.data)
+			//response.send(file.readFileSync(imgPath))
+			response.send({
+				name: role.name,
+				img: file.readFileSync(imgPath)
+			})
+		}
+		else if (err)
+	      console.log('res: ' + err)
+	});
 })
 
 app.get('/positions', function(req, res) {
@@ -119,10 +151,10 @@ process.argv.forEach((val, index, array) => {
     }
 })
   
-if (envMode === env.Local)
-    port = 80
-else
-    port = process.env.PORT
+//if (envMode === env.Local)
+    port = 1000
+//else
+    //port = process.env.PORT
 
 app.listen(port, function (error) {
     if(!error)
@@ -130,3 +162,37 @@ app.listen(port, function (error) {
     else
         console.log('error on find server inicialization: '+error)
 })
+
+//////////////////////////////////////////////////////////
+// data base operactions
+//////////////////////////////////////////////////////////
+
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://injoyserver:Yjjbr7SeP03oIKbHgvRAvO8lzEB4U2NNlPGx9IDIiqt3dnq5QQ32SjRVvQY3GFjhYOQf0gpCR393jiqrjnAawQ%3D%3D@injoyserver.documents.azure.com:10255/injoy?ssl=true', function(err, res) {
+  if ( err )
+    console.log('error: '+err);
+  else
+    console.log('Mongoose is connected to MongoDb on 27017');
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongo');
+});
+
+var roleSchema = mongoose.Schema({
+    name: String,
+	img: { data: Buffer, contentType: String }
+});
+
+var RoleModel = mongoose.model('role', roleSchema);
+
+var imgPath = './bart-icon.png';
+
+var ap11 = new RoleModel({name: 'ap11', img: { data: file.readFileSync(imgPath), contentType: 'image/png' } });
+
+// ap11.save(function(err) {
+// 	if (err) throw err;
+// });
