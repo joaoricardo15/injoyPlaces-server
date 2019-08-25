d72bb7aaccae22e8df69c083363854d94fac6f3c
joaoricardo15 <joaoricardo15@hotmail.com>
2019-08-25 18:11:38 -0300

    address fix

--- server.js
+++ server.js
@@ -465,22 +465,16 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() { console.log('connected to mongo') });
 
-var imgSchema = mongoose.Schema({ data: Buffer, contentType: String })
 var locationSchema = mongoose.Schema({ lat: Number, lng: Number })
+var addressSchema = mongoose.Schema({ street: String, number: Number, complement: String,  suburb: String, city: String, state: String, country: String })
 var rattingSchema = mongoose.Schema({ rattings: Number, average: Number })
-
-var userSchema = mongoose.Schema({
-	user: String,
-	email: String
-});
-
-var UserModel = mongoose.model('users', userSchema);
+var imgSchema = mongoose.Schema({ data: Buffer, contentType: String })
 
 var roleSchema = mongoose.Schema({
 	name: String,
 	ratting: rattingSchema,
 	location: locationSchema,
-	address: String,
+	address: addressSchema,
 	pic: Number,
 	pics: [ imgSchema ],
 	comments: [ String ],
@@ -488,14 +482,12 @@ var roleSchema = mongoose.Schema({
 	tags: [ String ]
 })
 
-var RoleModel = mongoose.model('roles', roleSchema);
-
 var experienceSchema = mongoose.Schema({
 	user: String,
 	name: String,
 	ratting: Number,
 	location: locationSchema,
-	address: String,
+	address: addressSchema,
 	date: Date,
 	pic: imgSchema,
 	comment: String,
@@ -503,13 +495,27 @@ var experienceSchema = mongoose.Schema({
 	tag: String
 })
 
+var userSchema = mongoose.Schema({ 
+	user: String,
+	email: String
+})
+
+var UserModel = mongoose.model('users', userSchema);
+var RoleModel = mongoose.model('roles', roleSchema);
 var ExperienceModel = mongoose.model('experiences', experienceSchema);
 
 // var ap11Model = new RoleModel({
 // 	name: 'Ap11',
 // 	ratting: { rattings: 1, average: 5 },
 // 	location: { lat: -30.039171, lng: -51.220676 },
-// 	address: 'R. General Lima e Silva, 697 - Cidade Baixa, Porto Alegre - RS',
+// 	address: { 
+// 		street: 'R. General Lima e Silva',
+// 		number: 697,
+// 		suburb: 'Cidade Baixa',
+// 		city: 'Porto Alegre',
+// 		state: 'RS',
+// 		country: 'Brasil'
+// 	},
 // 	pics: [ { data: file.readFileSync("./images/bars/ap11.jpg"), contentType: 'image/jpg' } ],
 // 	pic: 0,
 // 	comments: [],
@@ -517,26 +523,40 @@ var ExperienceModel = mongoose.model('experiences', experienceSchema);
 // 	tags: []
 // })
 // var redDoor = new RoleModel({
-//  name: 'Red Door',
-//  ratting: { rattings: 1, average: 4 },
-//  location: { lat: -30.041674, lng: -51.221539 },
-//  address: 'R. José do Patrocínio, 797 - Cidade Baixa, Porto Alegre - RS',
+// 	name: 'Red Door',
+// 	ratting: { rattings: 1, average: 4 },
+// 	location: { lat: -30.041674, lng: -51.221539 },
+// 	address: {
+// 		street: 'R. José do Patrocínio', 
+// 		number: 797,
+// 		suburb: 'Cidade Baixa',
+// 		city: 'Porto Alegre',
+// 		state: 'RS',
+// 		country: 'Brasil'
+// 	},
 // 	pics: [ { data: file.readFileSync("./images/bars/redDoor.jpg"), contentType: 'image/jpg' } ],
 // 	pic: 0,
 // 	comments: [],
 // 	occasions: [],
-//  tags: []
+// 	tags: []
 // })
 // var voidModel = new RoleModel({
-//  name: 'Void',
-//  ratting: { rattings: 1, average: 4 },
-//  location: { lat: -30.024672, lng: -51.203145 },
-// 	address: 'R. Luciana de Abreu, 364 - Moinhos de Vento, Porto Alegre - RS',
+// 	name: 'Void',
+// 	ratting: { rattings: 1, average: 4 },
+// 	location: { lat: -30.024672, lng: -51.203145 },
+// 	address: {
+// 		street: 'R. Luciana de Abreu',
+// 		number: 364,
+// 		suburb: 'Moinhos de Vento',
+// 		city: 'Porto Alegre',
+// 		state: 'RS',
+// 		country: 'Brasil'
+// 	},
 // 	pics: [ { data: file.readFileSync("./images/bars/void.jpg"), contentType: 'image/jpg' } ],
-//  pic: 0,
+// 	pic: 0,
 // 	comments: [],
 // 	occasions: [],
-//  tags: []
+// 	tags: []
 // })
 // ap11Model.save(function(err) { if (err) throw err })
 // redDoor.save(function(err) { if (err) throw err })
